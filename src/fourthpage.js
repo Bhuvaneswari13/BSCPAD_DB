09bf27889820a4098100ac5389e4c3fad74badf4
Bhuvaneswari13 <bhuvantnj13@gmail.com>
2021-07-28 10:23:48 +0530

    Changes-1

--- src/fourthpage.js
+++ src/fourthpage.js
@@ -3,90 +3,83 @@ import './App.css';
 //Calling Bootstrap 4.5 css
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fireDB from'./firebase';
+import { data } from 'jquery';
 class fourthpage extends React.Component {
   constructor(props) {
       
       super(props);
      
-      this.state = {Con : []}
+      this.state = {Con : []};
+      //this.state = {Con1: []};
       }
       componentDidMount() {
    
    
-     
-        fireDB.database().ref("Con").on("value", snapshot => {
-          let Con = [];
-          snapshot.forEach(snap => {
-              // snap.val() is the dictionary with all your keys/values from the 'students-list' path
-              Con.push(snap.val());
-          });
-          this.setState({ Con: Con });
+      
+      fireDB.database().ref("Con").orderByChild("No").equalTo(1).on("value", snapshot => {
+       let Con = [];
+       snapshot.forEach(snap => {
+            // snap.val() is the dictionary with all your keys/values from the 'students-list' path
+            Con.push(snap.val());
         });
+      this.setState({ Con: Con });
+     });  
       
+     /*fireDB.database().ref("Con").orderByChild("No").equalTo(2).on("value", snapshot => {
+       let Con1 = [];
+       snapshot.forEach(snap => {
+            // snap.val() is the dictionary with all your keys/values from the 'students-list' path
+            Con1.push(snap.val());
+        });
+      this.setState({ Con: Con1 });
+     }); */ 
       
-   }
+ }
+ 
+  
     
-    render(){
-    return (
-      <div className="MainDiv"> 
-       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-      integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"/>
-   
-        <div class="jumbotron text-center ">
-            <h3>Printing data from Firebase</h3>
-        </div>
-        <div className="container">   
-        
-        <div className='row'> 
-        <div className='col-xl-12'>
-        { this.state.Con.map(data => 
-            <div className="card float-left bg-sky text-dark" style={{width: '18rem', marginRight: '1rem', marginBlockStart: '1rem' }}>
-              <div className="card-body">
-                <h5 className="card-title">At : { data.At }</h5>
-                <h5 className="card-text">Balance : { data.Balance }</h5>
-                <h5 className="card-text">Decimal : { data.Decimal }</h5>
-                <h5 className="card-text">Name : { data.Name }</h5>
-                <h5 className="card-text">No : { data.No }</h5>
-                <h5 className="card-text">Symbol : { data.Symbol }</h5>
-                <h5 className="card-text">Total Supply : { data.ggg }</h5>
-                </div> 
-            </div> 
-            )
-        } 
-        </div>
-      </div> 
-       </div>
+ render(){
+  return (
+    <div className="MainDiv"> 
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+    integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"/>
+ 
+      <div class="jumbotron text-center ">
+          <h3>Printing data from Firebase</h3>
       </div>
-    );
-  }
-  }
-  export default fourthpage;
+      <div className="container">   
+      
+      <div className='row'> 
+      <div className='col-xl-12'>
 
-
-
-
-
-
-
-
-
-
-
-
-
-  {/* <div class="card bg-info text-white">
-    <div class="card-body">
-    {this.state.Student.map(data => {
-                  
-                  return ( <div>
-                    <h2>FirstName: {data.Fname} </h2>
-                    <h2>LastName: {data.Lname} </h2>
-                      </div>
-                  );
-                 
-                  })}
+     {this.state.Con.map(data => 
+          <div className="card float-left bg-sky text-dark" style={{width: '18rem', marginRight: '1rem', marginBlockStart: '1rem' }}>
+            <div className="card-body">
+              <h5 className="card-title">At : { data.At }</h5>
+              <h5 className="card-text">Balance : { data.Balance }</h5>
+              <h5 className="card-text">Decimal : { data.Decimal }</h5>
+              <h5 className="card-text">Name : { data.Name }</h5>
+              <h5 className="card-text">No : { data.No }</h5>
+              <h5 className="card-text">Symbol : { data.Symbol }</h5>
+              <h5 className="card-text">Total Supply : { data.TotalSupply }</h5>
+              </div> 
+          </div> 
+          )
+      }
+      
+      </div>
+    </div> 
+     </div>
     </div>
-      </div> <br/>
-                */} 
+  );
+}
+}
+export default fourthpage;
+  
+  
+  
+  
 
 
+
+ 
